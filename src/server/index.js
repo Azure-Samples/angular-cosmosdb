63cbe6bcde717f36e0ef19464e29beadb9c48adb
John Papa <johnpapa@users.noreply.github.com>
2017-09-27 13:20:18 -0400

    Sec refactor (#9)
    * default to development env.
    
    created NODE_ENV=production mode
    
    * prod node_env fod coker compose
    
    * update env details
    
    * fix typo
    
    * refactor server security code
    
    * remove commented out code
    
    * comments
    
    * consolidate
    
    * consolidated ... again
    
    * abstracted passport and other security jank to its own file

--- src/server/index.js
+++ src/server/index.js
@@ -1,41 +1,16 @@
 const express = require('express');
+const security = require('./security');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const csrf = require('csurf');
-const passport = require('passport');
-const helmet = require('helmet');
-const TwitterStrategy = require('passport-twitter').Strategy;
 
 const env = require('./env/' + (process.env.NODE_ENV || 'development'));
-
 const routes = require('./routes');
-
 const publicweb = process.env.PUBLICWEB || './publicweb';
-const app = express();
 
-app.use(helmet());
-app.use(cookieParser());
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(
-  session({
-    secret: env.sessionSecret,
-    resave: true,
-    saveUninitialized: true
-  })
-);
 
-app.use(csrf());
-app.use((req, res, next) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  return next();
-});
-
-// Initialize Passport
-// and restore authentication state, if any, from the session.
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(security());
 
 app.use(express.static(publicweb));
 console.log(`serving ${publicweb}`);
@@ -44,24 +19,4 @@ app.get('*', (req, res) => {
   res.sendFile(`index.html`, { root: publicweb });
 });
 
-if (env.twitter.consumerKey) {
-  passport.use(
-    new TwitterStrategy(
-      {
-        consumerKey: env.twitter.consumerKey,
-        consumerSecret: env.twitter.consumerSecret,
-        callbackURL: env.twitter.callbackURL
-      },
-      (token, tokenSecret, profile, done) => {
-        // find the user in this app's database using their twitter account
-        console.log(profile);
-        return done(null, profile);
-      }
-    )
-  );
-}
-
-console.log(env.twitter.callbackURL);
-
-const port = env.serverPort;
-app.listen(port, () => console.log(`API running on http://localhost:${port}`));
+app.listen(env.serverPort, () => console.log(`API running on http://localhost:${env.serverPort}`));
