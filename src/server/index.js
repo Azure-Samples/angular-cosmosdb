d86fff73ca0f419dc9915121f4a54eb813084176
clarkio <brian@clarkio.com>
2017-09-27 00:26:26 -0400

    remove commented out code

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-// const TwitterStrategy = require('passport-twitter').Strategy;
 const passportconfig = require('./config/passport');
 const session = require('express-session');
 const csrf = require('csurf');
