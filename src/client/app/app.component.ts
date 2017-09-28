8c7420cb092ac68d3c515815e4152c21380745ec
John Papa <john@Johns-MBP.jetpack>
2017-09-27 21:49:27 -0400

    !

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <app-toast></app-toast>
     <h1>
-      Heroes
+      Heroes!
     </h1>
     <app-login></app-login>
     <div class="header-bar"></div>
