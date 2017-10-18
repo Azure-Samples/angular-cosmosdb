c915ef3991644ccf25ac2dd1eea77f8a809098d5
John Papa <john@Johns-MBP.lan>
2017-10-18 16:36:28 -0400

    retitle

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   template: `
     <app-toast></app-toast>
     <h1>
-      Our Heroes
+      Nick's Heroes
     </h1>
     <app-login></app-login>
     <div class="header-bar"></div>
