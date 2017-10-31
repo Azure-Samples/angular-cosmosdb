c7da2d49eb3d52243e0786fea874eadaab4c2d75
John Papa <john@Johns-MacBook-Pro.local>
2017-10-31 14:13:36 -0700

    title

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>
-      Heroes
+      Anglebrackets Heroes
     </h1>
     <div class="header-bar"></div>
     <app-heroes></app-heroes>
