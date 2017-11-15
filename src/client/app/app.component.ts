c5665f1a745e8b5ad12a0f5dc16a1f516a061bfb
John Papa <john@DESKTOP-9RU0F2H.fareast.corp.microsoft.com>
2017-11-15 13:24:27 -0500

    trimming

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -3,13 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <app-toast></app-toast>
     <h1>{{ title }}</h1>
-    <app-login></app-login>
     <div class="header-bar"></div>
     <app-hero-list></app-hero-list>
   `
 })
 export class AppComponent {
-  title = 'Hi live 360';
+  title = 'Angular Heroes';
 }
