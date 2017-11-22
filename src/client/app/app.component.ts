6f73a85e2eed3cbd58bb9c4b32c1e2f2deedc929
John Papa <john@Johns-MacBook-Pro.local>
2017-11-21 22:13:22 -0500

    div

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -3,9 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+  <div>
     <h1>{{ title }}</h1>
     <div class="header-bar"></div>
     <app-hero-list></app-hero-list>
+  </div>
   `
 })
 export class AppComponent {
