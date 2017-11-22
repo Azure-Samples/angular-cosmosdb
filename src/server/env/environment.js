95a3f948be51b111268dc11de029a704e8f7b42e
John Papa <john@Johns-MacBook-Pro.local>
2017-11-21 21:30:57 -0500

    prettier

---
+++ src/server/env/environment.js
@@ -0,0 +1,8 @@
+// Replace with your dev values
+
+module.exports = {
+  accountName: 'my-heroes-db',
+  databaseName: 'admin',
+  key: 'VBXJsJKnmpd4r1LiDBlxWru5XZBdMcJ4hq8DTDOLAogojOsiw74rztcXvWcX5yIJcF8CLp2bGA2iwtRFimRDvg==',
+  port: 10255
+};
